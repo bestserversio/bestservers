92696737b3c63c10dfc56e0381b57a224cb66ed6
Christian Deacon <christianmdeacon@gmail.com>
2023-09-20 09:46:06 +0000

    Add platform rows and grid.

---
+++ src/components/platforms/Row.tsx
@@ -0,0 +1,55 @@
+import Markdown from "@components/markdown/Markdown";
+import { type Platform } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PlatformRow({
+    platform,
+    defaultBanner
+} : {
+    platform: Platform
+    defaultBanner?: string
+}) {
+    const uploadsUrl = process.env.NEXT_PUBLIC_UPLOADS_URL ?? "";
+
+    let banner = defaultBanner;
+
+    if (platform.banner)
+        banner = uploadsUrl + platform.banner;
+
+    const viewUrl = `/platforms/view/${platform.url}`;
+
+    return (
+        <div 
+            className="platform-row"
+            onClick={() => {
+                if (typeof window !== "undefined")
+                    window.location.href = viewUrl;  
+            }}
+        >
+            <div>
+                <div>
+                    {banner && (
+                        <Image
+                            src={banner}
+                            width={500}
+                            height={300}
+                            alt="Game Banner"
+                            objectFit="cover"
+                        />
+                    )}
+                </div>
+                <div>
+                    <h3>{platform.name}</h3>
+                </div>
+                <div>
+                    {platform.description && (
+                        <Markdown>
+                            {platform.description}
+                        </Markdown>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
