5af973cb7936bb609b4a79366b4bb30ed5823961
Christian Deacon <christianmdeacon@gmail.com>
2023-09-20 09:49:00 +0000

    Make progress on platform pages.

---
+++ src/pages/platforms/index.tsx
@@ -0,0 +1,91 @@
+import Meta from "@components/Meta";
+import Wrapper from "@components/Wrapper";
+import PlatformRow from "@components/platforms/Row";
+import { Platform, PlatformFlag } from "@prisma/client";
+import { prisma } from "@server/db";
+import { GetServerSidePropsContext } from "next";
+
+export default function Page({
+    games,
+    coms
+} : {
+    games?: Platform[]
+    coms?: Platform[]
+}) {
+    const defaultGameBanner = process.env.NEXT_PUBLIC_DEFAULT_PLATFORM_GAME_BANNER;
+    const defaultComBanner = process.env.NEXT_PUBLIC_DEFAULT_PLATFORM_COM_BANNER;
+
+    return (
+        <>
+            <Meta
+                title="Platforms & Games - Best Servers"
+                contentTitle="Platforms & Games"
+            />
+            <Wrapper>
+                {games && games.length > 0 && (
+                    <>
+                        <h2>Games</h2>
+                        <div className="platforms-grid">
+                            {games.map((game, index) => {
+                                return (
+                                    <PlatformRow
+                                        platform={game}
+                                        defaultBanner={defaultGameBanner}
+                                        key={`game-${index.toString()}`}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </>
+                )}
+
+                {coms && coms.length > 0 && (
+                    <>
+                        <h2>Communication Platforms</h2>
+                        <div className="platforms-grid">
+                            {coms.map((com, index) => {
+                                return (
+                                    <PlatformRow
+                                        platform={com}
+                                        defaultBanner={defaultComBanner}
+                                        key={`com-${index.toString()}`}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </>
+                )}
+            </Wrapper>
+        </>
+    );
+}
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+    const games = await prisma.platform.findMany({
+        where: {
+            flags: {
+                hasSome: [
+                    PlatformFlag.A2S
+                ]
+            }
+        }
+    });
+
+    const coms = await prisma.platform.findMany({
+        where: {
+            flags: {
+                hasSome: [
+                    PlatformFlag.DISCORD,
+                    PlatformFlag.TEAMSPEAK3
+                ]
+            }
+        }
+    });
+
+    return {
+        props: {
+            games: games,
+            coms: coms
+        }
+    }
+}
---
+++ src/pages/platforms/view/[platform].tsx
@@ -0,0 +1,60 @@
+import Meta from "@components/Meta";
+import Wrapper from "@components/Wrapper";
+import NotFound from "@components/statements/NotFound";
+import { Platform } from "@prisma/client";
+import { prisma } from "@server/db";
+import { GetServerSidePropsContext } from "next";
+
+export default function ({
+    platform
+} : {
+    platform?: Platform
+}) {
+    return (
+        <>
+            <Meta
+                title={`Viewing Platform - Best Servers`}
+                contentTitle={`Platform`}
+            />
+            <Wrapper>
+                {platform ? (
+                    <>
+                        <h1>{platform.name}</h1>
+                        <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
+                            <div className="flex flex-col gap-4 col-span-1 sm:col-span-3">
+                                <h2>Main</h2>
+                            </div>
+                            <div className="flex flex-col gap-4 col-span-1 sm:col-span-1">
+                                <h2>Sidebar</h2>
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <NotFound item="platform" />
+                )}
+            </Wrapper>
+        </>
+    );
+}
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+    const { params } = ctx;
+
+    const url = params?.platform?.toString();
+
+    let platform: Platform | null = null;
+
+    if (url) {
+        platform = await prisma.platform.findFirst({
+            where: {
+                url: url
+            }
+        });
+    }
+
+    return {
+        props: {
+            platform: platform
+        }
+    }
+}
