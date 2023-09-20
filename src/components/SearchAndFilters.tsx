cb3d3bb1a728c6e71739aaafd37c8ccb808da776
Christian Deacon <christianmdeacon@gmail.com>
2023-09-20 09:44:55 +0000

    Adjust search and filters component; Remove platforms box and adjust sort & sort dir types.

--- src/components/SearchAndFilters.tsx
+++ src/components/SearchAndFilters.tsx
@@ -1,17 +1,13 @@
 import React, { createContext, useState } from "react";
 
-import { type Category } from "@prisma/client";
-
 type filters = {
-    categories?: number[]
     sort?: string
     sortDir?: string
     online?: boolean
     search?: string,
 
-    setCategories?: React.Dispatch<React.SetStateAction<number[] | undefined>>
-    setSort?: React.Dispatch<React.SetStateAction<string | undefined>>
-    setSortDir?: React.Dispatch<React.SetStateAction<string | undefined>>
+    setSort?: React.Dispatch<React.SetStateAction<string>>
+    setSortDir?: React.Dispatch<React.SetStateAction<string>>
     setOnline?: React.Dispatch<React.SetStateAction<boolean | undefined>>
     setSearch?: React.Dispatch<React.SetStateAction<string | undefined>>
 }
@@ -19,60 +15,68 @@ type filters = {
 export const SearchAndFiltersCtx = createContext<filters | null>(null);
 
 export default function SearchAndFilters ({
-    categoriesList,
     children
 } : {
-    categoriesList?: Category[]
     children?: React.ReactNode
 }) {
-    const [categories, setCategories] = useState<number[] | undefined>(undefined);
-    const [sort, setSort] = useState<string | undefined>(undefined);
-    const [sortDir, setSortDir] = useState<string | undefined>(undefined);
+    const [sort, setSort] = useState<string>("curUsers");
+    const [sortDir, setSortDir] = useState<string>("desc");
     const [online, setOnline] = useState<boolean | undefined>(undefined);
     const [search, setSearch] = useState<string | undefined>(undefined);
 
     return (
         <div className="search-and-filters">
             <div>
-                <form>
-                    <div className="w-1/3">
-                        <label>Platforms</label>
-                        <select
-                            multiple={true}
-                            value={categories?.toString()}
-                        >
-                            {categoriesList?.map((category, index) => {
-                                return (
-                                    <option
-                                        value={category.id}
-                                        key={`category-${index.toString()}`}
-                                    >{category.name}</option>
-                                );
-                            })}
-                        </select>
-                    </div>
-                    <div className="w-2/3">
-                        <label>Search</label>
-                        <input
-                            value={search}
-                            onChange={(e) => {
-                                const val = e.currentTarget.value;
+                <div className="form">
+                    <div>
+                        <div>
+                            <label>Sort</label>
+                            <select
+                                onChange={(e) => {
+                                    const val = e.target.value;
 
-                                setSearch(val);
-                            }}
-                        />
+                                    setSort(val);
+                                }}
+                            >
+                                <option value="curUsers" selected={sort == "curUsers"}>Current Users</option>
+                                <option value="maxUsers" selected={sort == "maxUsers"}>Maximum Users</option>
+                                <option value="avgUsers" selected={sort == "avgUsers"}>Average Users</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label>Sort Direction</label>
+                            <select
+                                onChange={(e) => {
+                                    const val = e.target.value;
+
+                                    setSortDir(val);
+                                }}
+                            >
+                                <option value="asc" selected={sortDir == "asc"}>Ascending</option>
+                                <option value="desc" selected={sortDir == "desc"}>Desending</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label>Search</label>
+                            <input
+                                value={search}
+                                onChange={(e) => {
+                                    const val = e.currentTarget.value;
+
+                                    setSearch(val);
+                                }}
+                            />
+                        </div>
                     </div>
-                </form>
+                </div>
             </div>
             <div>
                 <SearchAndFiltersCtx.Provider value={{
-                    categories: categories,
                     sort: sort,
                     sortDir: sortDir,
                     online: online,
                     search: search,
 
-                    setCategories: setCategories,
                     setSort: setSort,
                     setSortDir: setSortDir,
                     setOnline: setOnline,
