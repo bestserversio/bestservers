8def29dddb1b1dbec4348dc80b6881b0c6fe146c
Christian Deacon <christianmdeacon@gmail.com>
2023-09-26 07:40:06 +0000

    Create components for different button types.

---
+++ src/components/buttons/GamePlayer.tsx
@@ -0,0 +1,35 @@
+import { GameplayerCtx } from "@components/GamePlayer";
+import { Server } from "@prisma/client";
+import { useContext } from "react";
+import { ServerPublic } from "~/types/Server";
+
+export default function GamePlayerButton ({
+    server,
+    className = "button"
+} : {
+    server: ServerPublic | Server
+    className?: string
+}) {
+    const gameplayerCtx = useContext(GameplayerCtx);
+
+    return (
+        <>
+            {"platform" in server && (server.platform?.jsInternal || server.platform?.jsExternal) && (
+                <button
+                    onClick={() => {
+                        if (!gameplayerCtx)
+                            return;
+
+                        gameplayerCtx.setVisible(true);
+
+                        if (server.platform?.jsInternal)
+                            gameplayerCtx.setInternal(server.platform.jsInternal);
+                        else if (server.platform?.jsExternal)
+                            gameplayerCtx.setExternal(server.platform.jsExternal);
+                    }}
+                    className={className}
+                >Game Player</button>
+            )}
+        </>
+    )
+}
---
+++ src/components/buttons/Join.tsx
@@ -0,0 +1,31 @@
+import { PlatformFlag, Server } from "@prisma/client";
+import Link from "next/link";
+import { ServerPublic } from "~/types/Server";
+
+export default function JoinButton ({
+    server,
+    className = "button"
+} : {
+    server: ServerPublic | Server
+    className?: string
+}) {
+    let joinUrl: string | undefined = undefined;
+
+    if ("platform" in server) {
+        if (server.platform?.flags.includes(PlatformFlag.A2S))
+            joinUrl = `steam://connect/${server.ip}:${server.port?.toString()}`
+        else if (server.platform?.flags.includes(PlatformFlag.DISCORD) && server.hostName)
+            joinUrl = server.hostName;
+    }
+
+    return (
+        <>
+            {joinUrl && (
+                <Link
+                    href={joinUrl}
+                    className={className}
+                >Join</Link>
+            )}
+        </>
+    )
+}
