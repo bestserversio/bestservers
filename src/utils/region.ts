3b52286543ffc91659339a0ddfba52c69c97d5c5
Christian Deacon <christianmdeacon@gmail.com>
2023-09-26 07:37:17 +0000

    Add two functions to region utils; GetRegionFromString() that retrieves Region enum depending on string and GetRegionFullName() that retrieves the region's full name.

--- src/utils/region.ts
+++ src/utils/region.ts
@@ -26,3 +26,58 @@ export function GetRegionFlag(region: Region): string {
             return basePath + "an.png";
     }
 }
+
+export function GetRegionFullName(region: Region): string {
+    switch (region) {
+        case Region.AF:
+            return "Africa";
+        
+        case Region.AS:
+            return "Asia";
+
+        case Region.EU:
+            return "Europe";
+
+        case Region.NA:
+            return "North America";
+
+        case Region.SA:
+            return "South America";
+        
+        case Region.OC:
+            return "Oceania";
+
+        case Region.AN:
+            return "Antarctica";
+    }
+}
+
+export function GetRegionFromString(str?: string): Region | undefined {
+    if (!str)
+        return undefined;
+    
+    switch (str.toLowerCase()) {
+        case "af":
+            return Region.AF;
+
+        case "as":
+            return Region.AS;
+
+        case "eu":
+            return Region.EU;
+
+        case "na":
+            return Region.NA;
+
+        case "sa":
+            return Region.SA;
+
+        case "oc":
+            return Region.SA;
+
+        case "an":
+            return Region.AN;
+    }
+
+    return undefined;
+}
