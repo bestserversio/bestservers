d4a278712938333d25d5f6ab70e7c1b744482249
Christian Deacon <christianmdeacon@gmail.com>
2023-09-26 07:41:18 +0000

    Use new user count class util function to determine what classes to add to rendered user counts.

--- src/components/servers/PlayerCount.tsx
+++ src/components/servers/PlayerCount.tsx
@@ -1,40 +1,26 @@
 import { Server } from "@prisma/client";
+import { RetrieveUserCountClasses, RetrieveUserFullClasses } from "@utils/UserCountClasses";
 import { ServerPublic } from "~/types/Server";
 
-const colors: {[key: number]: string} = {
-    65: "text-orange-300",
-    80: "text-green-300",
-    100: "text-red-400"
-}
-
 export default function PlayerCount ({
-    server
+    server    
 } : {
     server: ServerPublic | Server
 }) {
-    let curUserClasses: string | undefined = undefined;
-    let avgUserClasses: string | undefined = undefined;
+    const fullClasses = RetrieveUserFullClasses();
 
-    // Get fill percentage.
-    const curUserFill = (server.curUsers / server.maxUsers) * 100;
-    const avgUserFill = (server.avgUsers / server.maxUsers) * 100;
-
-    for (const key in colors) {
-        const color = colors[key];
-
-        if (Number(key) <= curUserFill)
-            curUserClasses = color;
-
-        if (Number(key) <= avgUserFill)
-            avgUserClasses = color;  
-    }
+    const curUserClasses = RetrieveUserCountClasses(server.curUsers, server.maxUsers);
+    const avgUserClasses = RetrieveUserCountClasses(server.avgUsers, server.maxUsers);
 
     return (
         <div className="flex flex-col gap-2">
             <div className="flex gap-1">
                 <span className={curUserClasses}>{server.curUsers.toString()}</span>
                 <span>/</span>
-                <span className={colors[100]}>{server.maxUsers.toString()}</span>
+                <span className={fullClasses}>{server.maxUsers.toString()}</span>
+                {server.bots > 0 && (
+                    <span className="text-xs">{server.bots.toString()}</span>
+                )}
             </div>
             <div>
                 Avg <span className={avgUserClasses}>{server.avgUsers.toString()}</span>
