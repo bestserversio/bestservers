8ad2c67542d98421aabe9dae1124b3e74bdcc5dd
Christian Deacon <christianmdeacon@gmail.com>
2023-09-26 07:37:43 +0000

    Create function to calculate what classes to add to user counts when rendered.

---
+++ src/utils/UserCountClasses.ts
@@ -0,0 +1,24 @@
+const classes: {[key: number]: string} = {
+    100: "text-red-400",
+    80: "text-green-300",
+    65: "text-orange-300"
+}
+
+export function RetrieveUserCountClasses(users: number, maxUsers: number): string | undefined {
+    let ret: string | undefined = undefined;
+
+    const fill = (users / maxUsers) * 100;
+
+    for (const key in classes) {
+        const c = classes[key];
+
+        if (fill >= Number(key))
+            ret = c;
+    }
+
+    return ret;
+}
+
+export function RetrieveUserFullClasses(): string | undefined {
+    return classes[100];
+}
