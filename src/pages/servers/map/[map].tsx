528190127e95c3f61cca33e8f113cc566c5d6b29
Christian Deacon <christianmdeacon@gmail.com>
2023-09-26 07:38:42 +0000

    Add page route for finding servers with map name.

---
+++ src/pages/servers/map/[map].tsx
@@ -0,0 +1,37 @@
+import Meta from "@components/Meta"
+import Wrapper from "@components/Wrapper"
+import ServerBrowser from "@components/servers/Browser"
+import { GetServerSidePropsContext } from "next"
+
+export default function Page ({
+    mapName
+} : {
+    mapName?: string
+}) {
+    return (
+        <>
+            <Meta
+
+            />
+            <Wrapper>
+                <h1>Servers Running Map {mapName ?? "N/A"}</h1>
+                <ServerBrowser
+                    preFilterMapName={mapName}
+                    table={true}
+                />
+            </Wrapper>
+        </>
+    )
+}
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+    const { params } = ctx;
+
+    const mapName = params?.map?.toString();
+
+    return {
+        props: {
+            mapName: mapName ?? null
+        }
+    }
+}
