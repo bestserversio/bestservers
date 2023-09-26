6fcef7c147e513697ff012979831bd774a9ca68d
Christian Deacon <christianmdeacon@gmail.com>
2023-09-26 07:41:32 +0000

    Make progress on server graph component.

--- src/components/servers/Graph.tsx
+++ src/components/servers/Graph.tsx
@@ -2,10 +2,14 @@ import { api } from "@utils/api";
 import { useState } from "react";
 import { ServerPublic } from "~/types/Server";
 
+import d3 from "d3";
+
 export default function ServerGraph({
-    server
+    server,
+    className = "server-graph"
 } : {
     server: ServerPublic
+    className?: string
 }) {
     const [timeframe, setTimeframe] = useState<number | undefined>(0);
 
@@ -17,36 +21,49 @@ export default function ServerGraph({
     const stats = statsQuery.data;
 
     return (
-        <div className="server-graph">
-            <div>
-                <ul>
-                    <li
-                        onClick={() => {
-                            setTimeframe(0);
-                        }}
-                    >Daily</li>
-                    <li
-                        onClick={() => {
-                            setTimeframe(1);
-                        }}
-                    >Weekly</li>
-                    <li
-                        onClick={() => {
-                            setTimeframe(2);
-                        }}
-                    >Monthly</li>
-                    <li
-                        onClick={() => {
-                            setTimeframe(3);
-                        }}
-                    >Yearly</li>
-                    <li
-                        onClick={() => {
-                            setTimeframe(undefined);
-                        }}
-                    >All Time</li>
-                </ul>
-            </div>
-        </div>
+        <>
+            {stats && stats.length > 0 && (
+                <div className={className}>
+                    <div>
+                        <ul>
+                            <li
+                                onClick={() => {
+                                    setTimeframe(0);
+                                }}
+                                className={timeframe == 0 ? "active" : undefined}
+                            >Daily</li>
+                            <li
+                                onClick={() => {
+                                    setTimeframe(1);
+                                }}
+                                className={timeframe == 1 ? "active" : undefined}
+                            >Weekly</li>
+                            <li
+                                onClick={() => {
+                                    setTimeframe(2);
+                                }}
+                                className={timeframe == 2 ? "active" : undefined}
+                            >Monthly</li>
+                            <li
+                                onClick={() => {
+                                    setTimeframe(3);
+                                }}
+                                className={timeframe == 3 ? "active" : undefined}
+                            >Yearly</li>
+                            <li
+                                onClick={() => {
+                                    setTimeframe(undefined);
+                                }}
+                                className={timeframe == undefined ? "active" : undefined}
+                            >All Time</li>
+                        </ul>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            )} 
+
+        </>
     );
 }
