c0231a20204c60bc8ac40695d42fbafc58569cef
Christian Deacon <christianmdeacon@gmail.com>
2023-09-30 22:44:35 +0000

    Add util functions for platform API endpoints.

---
+++ src/utils/platforms/api.ts
@@ -0,0 +1,85 @@
+import { Platform, PlatformFlag } from "@prisma/client"
+import { prisma } from "@server/db"
+
+export type PlatformBodyT = {
+    flags?: string[]
+
+    banner?: string
+    icon?: string
+
+    url?: string
+    name?: string
+    nameShort?: string
+    description?: string
+
+    jsInternal?: string
+    jsExternal?: string
+}
+
+export type PlatformDataT = {
+    flags?: PlatformFlag[]
+
+    banner?: string
+    icon?: string
+
+    url?: string
+    name?: string
+    nameShort?: string
+    description?: string
+
+    jsInternal?: string
+    jsExternal?: string
+}
+
+export type PlatformWhereT = {
+    id?: number | string
+    url?: string
+}
+
+export async function FindPlatform (where: PlatformWhereT): Promise<Platform | null> {
+    const{ id, url } = where;
+
+    if (!id && !url)
+        return null;
+
+    return await prisma.platform.findFirst({
+        where: {
+            OR: [
+                {
+                    ...(id && {
+                        id: Number(id)
+                    })
+                },
+                {
+                    ...(url && {
+                        url: url
+                    })
+                }
+            ]
+        }
+    });
+}
+
+export async function UpdatePlatform (id: number, data: PlatformDataT): Promise<Platform | null> {
+    return await prisma.platform.update({
+        data: data,
+        where: {
+            id: id
+        }
+    })
+}
+
+export async function DeletePlatform (where: PlatformWhereT): Promise<Platform | null> {
+    const platform = await FindPlatform(where);
+
+    const platformId = platform?.id;
+
+    if (!platformId)
+        return null;
+
+    return await prisma.platform.delete({
+        where: {
+            id: platformId
+        }
+    })
+}
