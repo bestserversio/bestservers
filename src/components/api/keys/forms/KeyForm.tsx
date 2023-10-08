dce314c335f73c94624bcf6ce9c1da27deb472b2
Christian Deacon <christianmdeacon@gmail.com>
2023-10-08 08:27:29 +0000

    Start working on API key form.

---
+++ src/components/api/keys/forms/KeyForm.tsx
@@ -0,0 +1,104 @@
+import Switch from "@components/helpers/Switch";
+import { ErrorCtx, SuccessCtx } from "@pages/_app";
+import { ApiKey } from "@prisma/client";
+import { api } from "@utils/api";
+import { randomBytes } from "crypto";
+import { Field, Form, Formik } from "formik";
+import { useContext, useEffect, useState } from "react";
+
+export default function ApiKeyForm({
+    apiKey
+} : {
+    apiKey?: ApiKey
+}) {
+    const errorCtx = useContext(ErrorCtx)
+    const successCtx = useContext(SuccessCtx)
+
+    const errHandler = (opts: unknown) => {
+
+    }
+    const sucHandler = () => {
+
+    }
+
+    const add = api.api.add.useMutation({
+        onError: errHandler,
+        onSuccess: sucHandler
+    })
+    const update = api.api.update.useMutation({
+        onError: errHandler,
+        onSuccess: sucHandler
+    })
+
+    const [writeAccess, setWriteAccess] = useState(false);
+
+    const [key, setKey] = useState<string | undefined>(undefined)
+
+    useEffect(() => {
+        if (!key) {
+            const genKey = randomBytes(64).toString("hex")
+
+            setKey(genKey)
+        }
+    }, [key])
+    return (
+        <Formik
+            initialValues={{
+                host: apiKey?.host ?? "",
+                endpoint: apiKey?.endpoint ?? "",
+                limit: apiKey?.limit ?? 3000
+            }}
+            onSubmit={(values) => {
+                if (!key) {
+                    console.error("Key somehow null.")
+
+                    return;
+                }
+                
+                if (apiKey) {
+                    update.mutate({
+                        id: apiKey.id,
+                        host: values.host,
+                        endpoint: values.endpoint,
+                        writeAccess: writeAccess,
+                        limit: values.limit
+                    })
+                } else {
+                    add.mutate({
+                        key: key,
+                        host: values.host,
+                        endpoint: values.endpoint,
+                        writeAccess: writeAccess,
+                        limit: values.limit
+                    })
+                }
+            }}
+        >
+            <Form className="form">
+                <div>
+                    <label htmlFor="host">Host</label>
+                    <Field name="host" />
+                </div>
+                <div>
+                    <label htmlFor="endpoint">Endpoint</label>
+                    <Field name="endpoint" />
+                </div>
+                <div>
+                    <label htmlFor="writeAccess">Write Access</label>
+                    <Switch
+                        onChange={() => {
+                            setWriteAccess(!writeAccess)
+                        }}
+                    ></Switch>
+                </div>
+                <div>
+                    <label htmlFor="limit">Limit</label>
+                    <Field
+                        as="number"
+                        name="limit"
+                    />
+                </div>
+            </Form>
+        </Formik>
+    )
+}
