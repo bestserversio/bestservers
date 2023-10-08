3f2e493762f39a90ee9ed3bdf963d646743eb714
Christian Deacon <christianmdeacon@gmail.com>
2023-10-08 08:28:05 +0000

    Use tRPC for categories and add banner/icon support into category form.

--- src/components/categories/forms/Main.tsx
+++ src/components/categories/forms/Main.tsx
@@ -1,16 +1,14 @@
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, isObject } from "formik";
 
 import { type Category } from "@prisma/client";
 import { api } from "@utils/api";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ErrorCtx, SuccessCtx } from "@pages/_app";
 
 export default function CategoryForm({
-    category,
-    categories
+    category
 } : {
-    category?: Category,
-    categories: Category[]
+    category?: Category
 }) {
     const errorCtx = useContext(ErrorCtx);
     const successCtx = useContext(SuccessCtx);
@@ -21,15 +19,26 @@ export default function CategoryForm({
             const { message, data } = opts;
 
             if (errorCtx) {
-                
+                errorCtx.setTitle(`Failed To ${category ? "Save" : "Add"} Category`)
+
+                // To Do: Determine error message.
             }
         },
         onSuccess: () => {
             if (successCtx) {
-
+                errorCtx?.setTitle(`Successfully ${category ? "Saved" : "Added"} Category!`)
+                errorCtx?.setMsg(`The category was not ${category ? "saved" : "added"} successfully.`)
             }
         }
     });
+
+    // Retrieve categories mapped.
+    const catQuery = api.categories.allMapped.useQuery();
+    const categories = catQuery.data;
+
+    // Banners and icons.
+    const [banner, setBanner] = useState<string | ArrayBuffer | null>(null);
+    const [icon, setIcon] = useState<string | ArrayBuffer | null>(null);
 
     return (
         <Formik
@@ -37,26 +46,76 @@ export default function CategoryForm({
                 parent: category?.parentId ?? 0,
                 url: category?.url ?? "",
                 name: category?.name ?? "",
-                description: category?.description
+                description: category?.description ?? ""
             }}
             onSubmit={(values) => {
                 console.log(values);
+
+                addOrUpdate.mutate({
+                    ...values,
+                    parent: values.parent || null,
+                    description: values.description || null,
+                    id: category?.id,
+                    banner: banner?.toString(),
+                    icon: icon?.toString()
+                });
             }}
         >
             <Form>
                 <div>
+                    <label>Banner</label>
+                    <input
+                        type="file"
+                        onChange={(e) => {
+                            const file = e.target.files?.[0]
+
+                            if (file) {
+                                const reader = new FileReader()
+                                reader.readAsDataURL(file)
+
+                                reader.onload = () => {
+                                    setBanner(reader.result)
+                                }
+                            }
+                        }}
+                    />
+                </div>
+                <div>
+                    <label>Icon</label>
+                    <input
+                        type="file"
+                        onChange={(e) => {
+                            const file = e.target.files?.[0]
+
+                            if (file) {
+                                const reader = new FileReader()
+                                reader.readAsDataURL(file)
+
+                                reader.onload = () => {
+                                    setIcon(reader.result)
+                                }
+                            }
+                        }}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="parent">Parent</label>
                     <select
                         name="parent"
                     >
                         <option value="0">None</option>
-                        {categories.map((category, index) => {
-                            return (
-                                <option
-                                    value={category.id}
-                                    key={`server-${index.toString()}`}
-                                >{category.name}</option>
-                            );
-                        })}
+                        {categories && (
+                            <>
+                                {categories.map((category, index) => {
+                                    return (
+                                        <option
+                                            value={category.id}
+                                            key={`category-${index.toString()}`}
+                                        >{category.name}</option>
+                                    );
+                                })}
+                            </>
+                        )}
                     </select>
                 </div>
                 <div>
