e8302c49e5aa2a059278f7d72f83d8470cc96824
Christian Deacon <christianmdeacon@gmail.com>
2024-06-04 03:34:03 -0400

    Add sort arrow icon.

---
+++ src/components/icons/Sort.tsx
@@ -0,0 +1,11 @@
+export default function SortIcon({
+    className
+} : {
+    className?: string
+}) {
+    return (
+        <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 512">
+            <path d="m182.6 169.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9 5 12 16.6 19.8 29.6 19.8h256c12.9 0 24.6-7.8 29.6-19.8 5-12 2.2-25.7-6.9-34.9l-128-128z" />
+        </svg>
+    )
+}
