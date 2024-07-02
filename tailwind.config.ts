9941071f1b516ba6c33147f62ed1afae96e50c6d
Christian Deacon <christianmdeacon@gmail.com>
2024-07-01 22:05:22 -0400

    Add new colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,16 +5,21 @@ export default {
   theme: {
     extend: {
         "colors": {
-            "bs-header": "#000000"
+            "shade-0": "#0D2230",
+            "shade-1": "#0f2738",
+            "shade-2": "#1A4361",
+            "shade-3": "#1E4E71",
+            "shade-4": "#225981",
+            "shade-5": "#276591",
+            "shade-6": "#2B70A1",
+            "shade-7": "#2F7BB1",
+            "shade-8": "#3386C1",
+            "shade-9": "#3E90CC"
         },
         "animation": {
             "logo-slide-down": "logo-slide-down 1s ease-out",
             "logo-slide-up": "logo-slide-up 1s ease-out",
             "gameplayer-slide-in": "gameplayer-slide-in 1s ease-out"
-        },
-        "fontFamily": {
-            lilitaone: ["lilitaone"],
-            gruppo: ["gruppo"]
         }
     },
   },
