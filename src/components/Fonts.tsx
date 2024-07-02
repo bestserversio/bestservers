deec284409a4d7816ad4c98a28ec469e4a0e2481
Christian Deacon <christianmdeacon@gmail.com>
2024-07-01 22:10:43 -0400

    Update fonts.

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -1,4 +1,4 @@
-import { Black_Ops_One, Dosis, Exo, Heebo, Josefin_Sans, Mulish, Nunito, Plus_Jakarta_Sans, Raleway, Roboto_Mono, Roboto_Slab, Sora, Ubuntu } from "next/font/google";
+import { Black_Ops_One, Dosis, Exo, Heebo, Josefin_Sans, Kanit, Mulish, Nunito, Oswald, Permanent_Marker, Plus_Jakarta_Sans, Raleway, Roboto_Mono, Roboto_Slab, Sora, Ubuntu } from "next/font/google";
 
 export const FBlackOps = Black_Ops_One({ subsets: ["latin"], weight: "400" });
 export const FJakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
@@ -13,3 +13,7 @@ export const FMulish = Mulish({ subsets: ["latin"] });
 export const FHeebo = Heebo({ subsets: ["latin"] });
 export const FJosefin = Josefin_Sans({ subsets: ["latin"] });
 export const FDosis = Dosis({ subsets: ["latin"] });
+
+export const FKanit = Kanit({ subsets: ["latin"], weight: "700" })
+
+export const FPermanentMarker = Permanent_Marker({ subsets: ["latin"], weight: "400" })
