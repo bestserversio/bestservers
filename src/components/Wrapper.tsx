91d2060c3d54adadadefe2b6d3dd83064ed9b48c
Christian Deacon <christianmdeacon@gmail.com>
2024-07-01 22:06:14 -0400

    Add ability to disable background image.

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,10 +1,11 @@
-import React, { createContext, useContext, useEffect, useRef, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import Header from "./Header";
 import GoogleAnalytics from "./GoogleAnalytics";
 import { ErrorCtx, SuccessCtx } from "@pages/_app";
 import SuccessBox from "./statements/Success";
 import ErrorBox from "./statements/Error";
 import GamePlayer from "./GamePlayer";
+import { useCookies } from "react-cookie";
 
 const bgImages = [
     "csgo.jpg",
@@ -33,6 +34,8 @@ export default function Wrapper ({
     const errorCtx = useContext(ErrorCtx);
     const successCtx = useContext(SuccessCtx);
 
+    const [cookies] = useCookies(["bs_showbg"]);
+
     const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
@@ -54,11 +57,18 @@ export default function Wrapper ({
 
     const bgSpeed = process.env.NEXT_PUBLIC_BACKGROUND_SPEED ?? "15";
 
+    const [showBg, setShowBg] = useState(false);
+
+    useEffect(() => {
+        if (cookies["bs_showbg"] !== undefined)
+            setShowBg(Boolean(cookies["bs_showbg"]));
+    }, [cookies])
+
     const [curBg, setCurBg] = useState<string | undefined>(undefined);
 
     useEffect(() => {
         const switchBg = () => {
-            if (isMobile || typeof window === "undefined")
+            if (!showBg || isMobile || typeof window === "undefined")
                 return;
     
                 // Create a new array based off of our background images.
@@ -81,11 +91,14 @@ export default function Wrapper ({
             switchBg();
         }, Number(bgSpeed) * 1000)
 
-        if (!curBg)
+        if (!showBg && curBg !== undefined)
+            setCurBg(undefined);
+
+        if (!curBg && showBg)
             switchBg();
 
         return () => clearInterval(timerId);
-    }, [curBg, isMobile, bgImages])
+    }, [curBg, isMobile, bgImages, showBg])
 
     // Location.
     const [curLocation, setCurLocation] = useState<LocationT | undefined>(undefined);
@@ -104,22 +117,23 @@ export default function Wrapper ({
         }
     }, [])
 
+    const do_static_bg = isMobile || !showBg;
 
     return (
         <ViewPortCtx.Provider value={{
             isMobile: isMobile
         }}>
             <LocationCtx.Provider value={curLocation}>
-                <main className={isMobile ? "bg-gradient-to-b from-gray-900 to-gray-950" : undefined}>
+                <main className={do_static_bg ? "bg-gradient-to-b from-gray-900 to-gray-950" : undefined}>
                     <div
-                        className={`${isMobile ? "hidden" : ""} fixed top-0 left-0 -z-20 w-full h-full bg-cover`}
+                        className={`${do_static_bg ? "hidden" : ""} fixed top-0 left-0 -z-20 w-full h-full bg-cover`}
                         style={{
-                            backgroundImage: (!isMobile && curBg) ? `url('/images/background/${curBg}')` : undefined,
+                            backgroundImage: (!do_static_bg && curBg) ? `url('/images/background/${curBg}')` : undefined,
                             transition: "background-image ease-in-out 2s"
                         }}
                     />
                     <div
-                        className={`${isMobile ? "hidden" : ""} fixed top-0 left-0 -z-10 w-full h-full bg-black/90`}
+                        className={`${do_static_bg ? "hidden" : ""} fixed top-0 left-0 -z-10 w-full h-full bg-black/90`}
                     />
 
                     <Header />
