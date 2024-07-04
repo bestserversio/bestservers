9a83172210bb8070cd8f02c59421ef4b0dd0583a
Christian Deacon <christianmdeacon@gmail.com>
2024-07-03 21:18:15 -0400

    Display total servers on front page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 import Wrapper from "@components/Wrapper";
 
 import ServerBrowser from "@components/servers/Browser";
+import { prisma } from "@server/db";
 
-export default function Index() {
+export default function Index({
+    totalServers = 0
+} : {
+    totalServers?: number
+}) {
     return (
         <Wrapper>
-            <ServerBrowser table={true} />
+            <div className="flex flex-col gap-2">
+                <div className="flex justify-center">
+                    <h2>Tracking <span className="font-bold text-shade-9">{totalServers.toString()}</span> Servers!</h2>
+                </div>
+                <ServerBrowser table={true} />
+            </div>
         </Wrapper>
     );
+}
+
+export async function getServerSideProps() {
+    const totalServers = await prisma.server.count();
+
+    return {
+        props: {
+            totalServers: totalServers
+        }
+    }
 }
