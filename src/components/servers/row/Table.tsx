6cc6be1a37576dedd2499484cebaad2cbb35f391
Christian Deacon <christianmdeacon@gmail.com>
2024-07-06 20:30:26 -0400

    Increase opacity for offline servers.

--- src/components/servers/row/Table.tsx
+++ src/components/servers/row/Table.tsx
@@ -17,7 +17,7 @@ export default function ServerRowTable ({
     server: ServerPublic
 }) {
     const uploadsUrl = process.env.NEXT_PUBLIC_UPLOADS_URL;
-    const gameplayerCtx = useContext(GameplayerCtx);
+    //const gameplayerCtx = useContext(GameplayerCtx);
     
     let joinUrl: string | undefined = undefined;
 
@@ -62,7 +62,7 @@ export default function ServerRowTable ({
     }, [server.lastQueried])
 
     return (
-        <tr className={`server-row-table bg-gradient-to-b from-shade-2/70 to-shade-3/70 hover:duration-150 hover:from-shade-4/70 hover:to-shade-5/70 border-separate border-spacing-4 rounded-lg ${!server.online ? "opacity-30" : ""}`}>
+        <tr className={`server-row-table bg-gradient-to-b from-shade-2/70 to-shade-3/70 hover:duration-150 hover:from-shade-4/70 hover:to-shade-5/70 border-separate border-spacing-4 rounded-lg ${!server.online ? "opacity-60" : ""}`}>
             <td className={!server.rating ? "w-0" : "w-auto"}>
                 {server.rating && (
                     <span className="text-lg text-bold">{server.rating.toString()}</span>
