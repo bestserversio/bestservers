f922e0e066af0eb46b3ed3517d5e70cc45e0e5b3
Christian Deacon <christianmdeacon@gmail.com>
2024-07-06 22:22:30 -0400

    Make header nav items lighter.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -155,7 +155,7 @@ function NavItem({
         <Link
             href={url}
             onClick={onClick}
-            className={`${FCabin.className} text-lg duration-150 font-bold ${active ? "opacity-100": "opacity-80"} hover:opacity-100`}
+            className={`${FCabin.className} text-white text-lg duration-150 font-bold ${active ? "opacity-100": "opacity-80"} hover:opacity-100`}
             target={newTab ? "_blank" : undefined}
         >
             {children}
