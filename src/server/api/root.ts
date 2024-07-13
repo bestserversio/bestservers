0576391f1ad52d629e6ffea40efbf504eadc2323
Christian Deacon <christianmdeacon@gmail.com>
2024-07-13 02:14:24 -0400

    Add Spy router to root.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -4,12 +4,14 @@ import { serversRouter } from "./routers/servers";
 import { platformsRouter } from "./routers/platforms";
 import { categoriesRouter } from "./routers/categories";
 import { apiRouter } from "./routers/api";
+import { spyRouter } from "./routers/spy";
 
 export const appRouter = createTRPCRouter({
     platforms: platformsRouter,
     categories: categoriesRouter,
     servers: serversRouter,
-    api: apiRouter
+    api: apiRouter,
+    spy: spyRouter
 });
 
 export type AppRouter = typeof appRouter;
