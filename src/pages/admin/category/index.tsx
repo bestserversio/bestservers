57ab252ad8030a3b1c7fbaa90b67f74839a10863
Christian Deacon <christianmdeacon@gmail.com>
2024-07-12 22:22:01 -0400

    Make progress on category index admin page.

--- src/pages/admin/category/index.tsx
+++ src/pages/admin/category/index.tsx
@@ -8,11 +8,20 @@ import NoPermissions from "@components/statements/NoPermissions";
 import { isAdmin } from "@utils/auth";
 import Meta from "@components/Meta";
 import AdminMenu from "@components/admin/Menu";
+import { type CategoryWithChildren } from "~/types/Category";
+import { prisma } from "@server/db";
+import { type Category } from "@prisma/client";
+import Link from "next/link";
+import Image from "next/image";
+import { api } from "@utils/api";
+import { Dispatch, SetStateAction, useState } from "react";
 
 export default function Page ({
-    authed    
+    authed,
+    categories 
 } : {
     authed: boolean
+    categories: CategoryWithChildren[]
 }) {
     return (
         <>
@@ -22,7 +31,41 @@ export default function Page ({
             <Wrapper>
                 {authed ? (
                     <AdminMenu current="categories">
-                        <p>Placeholder.</p>
+                        <div className="flex flex-col gap-2">
+                            <h1>Categories</h1>
+                            <div>
+                                {categories.length > 0 ? (
+                                    <table className="table table-auto w-full">
+                                        <thead>
+                                            <tr>
+                                                <th></th>
+                                                <th></th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {categories.map((category, idx) => {
+                                                return (
+                                                    <ParentRow
+                                                        key={`pr-${idx.toString()}`}
+                                                        category={category}
+                                                    />
+                                                )
+                                            })}
+                                        </tbody>
+                                    </table>
+                                ) : (
+                                    <p>No categories found.</p>
+                                )}
+                            </div>
+                            <div className="flex justify-center">
+                                <Link
+                                    href="/admin/category/add"
+                                    className="button button-primary"
+                                >Add Category!</Link>
+                            </div>
+                        </div>
+                    
                     </AdminMenu>
                 ) : (
                     <NoPermissions />
@@ -32,14 +75,136 @@ export default function Page ({
     );
 }
 
+function ParentRow({
+    category
+} : {
+    category: CategoryWithChildren
+}) {
+    const [showChildren, setShowChildren] = useState(false);
+
+    return (
+        <>
+            <tr>
+                <CategoryInfo
+                    category={category}
+                    setShowChildren={setShowChildren}
+                    showChildren={showChildren}
+                    childrenCnt={category.children.length}
+                />
+            </tr>
+            {(category.children.length > 0 && showChildren) && (
+                <>
+                    {category.children.map((child, idx) => {
+                        return (
+                            <ChildRow
+                                key={`cr-${idx.toString()}`}
+                                category={child}
+                            />
+                        )
+                    })}
+                </>
+            )}
+        </>
+    )
+}
+
+function ChildRow({
+    category
+} : {
+    category: Category
+}) {
+    return (
+        <tr>
+            <CategoryInfo category={category} />
+        </tr>
+    )
+}
+
+function CategoryInfo({
+    category,
+    showChildren = false,
+    setShowChildren,
+    childrenCnt = 0
+} : {
+    category: CategoryWithChildren | Category
+    showChildren?: boolean
+    setShowChildren?: Dispatch<SetStateAction<boolean>>
+    childrenCnt?: number
+}) {
+    const uploadPath = process.env.NEXT_PUBLIC_UPLOADS_URL ?? "";
+
+    let icon = "/images/default_icon.png";
+
+    if (category.icon)
+        icon = uploadPath + category.icon;
+
+    const editUrl = `/admin/category/edit/${category.id.toString()}`;
+    const deleteMut = api.categories.delete.useMutation();
+
+    return (
+        <>
+            <td className="w-8">
+                <Image
+                    src={icon}
+                    height={32}
+                    width={32}
+                    alt="Category Icon"
+                />
+            </td>
+            <td>
+                <span>{category.name}</span>
+            </td>
+            <td>
+                <div className="flex gap-2">
+                    <Link
+                        href={editUrl}
+                        className="button button-primary"
+                    >Edit</Link>
+                    <button
+                        onClick={() => {
+                            const yes = confirm("Are you sure you want to delete this category?");
+
+                            if (yes) {
+                                deleteMut.mutate({
+                                    id: category.id
+                                });
+                            }
+                        }}
+                        className="button button-danger"
+                    >Delete</button>
+                    {(setShowChildren && childrenCnt > 0) && (
+                        <button
+                            onClick={() => setShowChildren(!showChildren)}
+                            className="button button-secondary"
+                        >{showChildren ? "Hide Children" : "Show Children"}</button>
+                    )}
+                </div>
+            </td>
+        </> 
+    )
+}
+
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
     const session = await getServerAuthSession(ctx);
 
     const authed = isAdmin(session);
+    let categories: CategoryWithChildren[] = [];
+
+    if (authed) {
+        categories = await prisma.category.findMany({
+            where: {
+                parent: null
+            },
+            include: {
+                children: true
+            }
+        })
+    }
 
     return {
         props: {
-            authed: authed
+            authed: authed,
+            categories: categories
         }
     }
 }
