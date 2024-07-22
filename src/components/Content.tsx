7ed517c080c45682eeab9abbe38c7b6d7557dfb5
Christian Deacon <christianmdeacon@gmail.com>
2024-07-21 23:11:16 -0400

    Only apply link colors to links inside of <p> elements.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -14,7 +14,7 @@ export function ContentItem1({
             {title && (
                 <h1>{title}</h1>
             )}
-            <div className="bg-shade-2/70 p-4 rounded-sm [&_a]:!text-cyan-400 [&_a:link]:!text-cyan-400 [&_a:hover]:!text-cyan-200 [&_p]:mb-4">
+            <div className="bg-shade-2/70 text-sm p-4 rounded-sm [&_p_a]:!text-cyan-400 [&_p_a:link]:!text-cyan-400 [&_p_a:hover]:!text-cyan-200 [&_p]:mb-4">
                 {children}
             </div>
         </div>
@@ -35,7 +35,7 @@ export function ContentItem2({
             {title && (
                 <h1>{title}</h1>
             )}
-            <div className="[&_p]:mb-4 [&_a]:!text-cyan-400 [&_a:link]:!text-cyan-400 [&_a:hover]:!text-cyan-200">
+            <div className="[&_p]:mb-4 text-sm [&_p_a]:!text-cyan-400 [&_p_a:link]:!text-cyan-400 [&_p_a:hover]:!text-cyan-200">
                 {children}
             </div>
         </div>
