3c838730671f2aa14c28836903f7c1acd0e52bc6
Christian Deacon <christianmdeacon@gmail.com>
2024-07-25 03:43:20 -0400

    Oops, fix error.

--- src/components/buttons/Join.tsx
+++ src/components/buttons/Join.tsx
@@ -12,7 +12,7 @@ export default function JoinButton ({
     let joinUrl: string | undefined = undefined;
 
     if ("platform" in server) {
-\        if (server.platform?.flags.includes("A2S"))
+        if (server.platform?.flags.includes("A2S"))
             joinUrl = `steam://connect/${server.ip}:${server.port?.toString()}`
         else if (server.platform?.flags.includes("DISCORD") && server.hostName)
             joinUrl = server.hostName;
