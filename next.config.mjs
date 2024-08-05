a16730476dbc0e7f9f3b7d780eb53c31c1c3a1fd
Christian Deacon <christianmdeacon@gmail.com>
2024-08-04 20:02:32 -0400

    Fix Next images.

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,16 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: '**',
+            port: '',
+            pathname: '**',
+        }
+    ]
+  },
   rewrites: async() => [
     {
       source: "/sitemap.xml",
